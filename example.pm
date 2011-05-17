bc8e904e800462770e65d9945a894d33930d3c05
Jack Richardson <jackr94@live.co.uk>
2011-05-17 17:30:23 +0000

    nano fails

--- example.pm
+++ example.pm
@@ -15,21 +15,4 @@ sub on_delete {
 
 }
 
-1#!/usr/bin/env perl
-# Example Module
-# Copyright 2011 Jack Richardson
-package example;
-
-# Register Functions to API
-&modAPI::register(\&on_register); 
-&modAPI::reg_delete(\&on_delete); 
-
-sub on_register {
-
-}
-
-sub on_delete {
-
-}
-
 1
