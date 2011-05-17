5c6ea678a81fa07c37568d3e5b9e6cbe042c7a52
Jack Richardson <jackr94@live.co.uk>
2011-05-17 17:29:03 +0000

    Wiped

--- example.pm
+++ example.pm
@@ -8,19 +8,28 @@ package example;
 &modAPI::reg_delete(\&on_delete); 
 
 sub on_register {
-	print "Example Module Loaded!\n";
-	$core::commands{"example"} = sub {
-		print "Example Handle has been executed!\n";
-	};
+
 }
 
 sub on_delete {
-	# Unload - EVERYTHING
-	delete $core::commands{"example"};
-	delete $core::modules{"example"};
-	delete $core::rm_mod{"example"};
-	Class::Unload->unload("example.pm");
-	print "Example Module Unloaded.";
+
+}
+
+1#!/usr/bin/env perl
+# Example Module
+# Copyright 2011 Jack Richardson
+package example;
+
+# Register Functions to API
+&modAPI::register(\&on_register); 
+&modAPI::reg_delete(\&on_delete); 
+
+sub on_register {
+
+}
+
+sub on_delete {
+
 }
 
 1
