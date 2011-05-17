f95b3220251f82d4f5879ab51e6e7271b9b4c4e6
Jack Richardson <jackr94@live.co.uk>
2011-05-17 15:57:56 +0000

    Finished API

--- API.pm
+++ API.pm
@@ -1,6 +1,7 @@
 #!/usr/bin/env perl
 # API Module Loader
 # Copyright 2011 Jack Richardson
+
 package modAPI;
 
 use Data::Dumper;
@@ -8,13 +9,21 @@ use Class::Unload;
 
 sub register {
 	my $mod = caller(0);
-	$core::modules{$mod} = $_[0];
-	$core::modules{$mod}(); # Execute the function
+	$core::modules{$mod} = $_[0] if $_[0];
+	$core::modules{$mod}() if $core::modules{$mod};
 	
 }
 sub reg_delete {
 	my $mod = caller(0);
-	$core::rm_mod{$mod} = $_[0];
+	$core::rm_mod{$mod} = $_[0] if $_[0];
+}
+
+sub unload {
+	my $mod = caller(0);
+	delete $core::commands{$mod} if exists $core::commands{$mod};
+	delete $core::modules{$mod} if exists $core::modules{$mod};
+	delete $core::rm_mod{$mod} if exists $core::rm_mod{$mod};
+	Class::Unload->unload($mod);
 }
 
 1
