7ffb12118192f4abcbb5462c1232de3d87457fe4
Jack Richardson <jackr94@live.co.uk>
2011-05-17 16:41:26 +0000

    Configuration Parsing Complete!

--- core.pl
+++ core.pl
@@ -10,15 +10,48 @@ use Data::Dumper;
 our %modules;
 our %rm_mod;
 our %commands;
+our %config;
 
-my $do = do "example.pm";
-if($do) {
-	print Dumper(\%commands);
-	print Dumper(\%rm_mod);
-	print Dumper(\%modules);
+# Parse & Validate Config
+%config = &configParse("bot.conf");
+&validate(\%config);
+
+
+
+sub configParse {
+	my($config, $readarg, @args) = ($_[0], "<", @_);
+	open(CONFIG, $readarg, $config) or die "$!\n";
+	while(<CONFIG>) {
+		chomp;
+		s/;.*//; # Parse Comments
+		s/config.*//;s/}.*//; # Parse Structure
+		s/"|"//;s/"|"//; # Parse Parenthesis
+		s/\s+$//; # Whitespace hax
+		s/^\s+//; # "            "
+		next unless length;
+		my($key, $config) = split (/\s*=\s*/, $_, 2);
+		$tmp{$key} = $config;
+	}
+	close(CONFIG);
+	return %tmp;
 }
 
-# Execute Loaded Command
-
-$commands{"example"}();
-$rm_mod{"example"}();
+sub validate {
+	my($err) = (0);
+	if( ! $config{'server'} ){ print "&core::validate => Server Not 
+defined in configuration"; $err = 1; }
+	if( ! $config{'port'} ){ print "&core::validate => Port Not 
+defined in configuration"; $err = 1; }
+	if( ! $config{'nickname'} ){ print "&core::validate => Nickname 
+Not defined in configuration"; $err = 1; }
+	if( ! $config{'username'} ){ print "&core::validate => Username 
+Not defined in configuration"; $err = 1; }
+	if( ! $config{'realname'} ){ print "&core::validate => Realname 
+Not defined in configuration"; $err = 1; }
+	if( ! $config{'autojoin'} ){ print "&core::validate => Server 
+Not defined in configuration"; $err = 1; }
+	if($err) {
+		print "\n\nexiting...\n";
+		exit(0);
+	}
+}
